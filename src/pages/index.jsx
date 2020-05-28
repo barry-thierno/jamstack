e7260fa2343327145e31eb0504aa7db57d6a5760
barry-thierno <thsaidbarry@gmail.com>
2020-05-28 14:05:22 +0200

    feat: fix jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,16 +4,18 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
